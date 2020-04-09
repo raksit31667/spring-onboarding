959b6dc40c9537e306a22bef1e84714de3f79ff1
Raksit <raksit.m@ku.th>
2020-04-09 12:24:48 +0700

    created repository connected with h2 db

--- src/main/java/com/raksit/springonboarding/service/CustomerService.java
+++ src/main/java/com/raksit/springonboarding/service/CustomerService.java
@@ -1,11 +1,16 @@
 package com.raksit.springonboarding.service;
 
+import com.raksit.springonboarding.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
+  @Autowired
+  private CustomerRepository customerRepository;
+
   public String findById(Long customerId) {
-    return "hello, " + customerId;
+    return "hello, " + customerRepository.findById(customerId).get().getName();
   }
 }
