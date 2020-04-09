e8c206cea31e687aaaec77a5342a2731f3c0cb31
Raksit <rmanta1@ap.xom.com>
2020-04-09 10:47:42 +0700

    :tada:

---
+++ src/main/java/com/raksit/springonboarding/SpringOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.raksit.springonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnboardingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringOnboardingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/raksit/springonboarding/SpringOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raksit.springonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnboardingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
